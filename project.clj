e5e5c53e94415d824f2f84a8bed741b4cce7c48b
Anh Nguyet Vu <anngvu@gmail.com>
2024-05-16 20:08:12 -0600

    Update deps and jvm opts

--- project.clj
+++ project.clj
@@ -3,12 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.csv "1.1.0"]
                  [cheshire "5.13.0"]
                  [org.clojars.huahaiy/datalevin-native "0.9.1"]
-                 [http-kit "2.8.0"]
+                 [org.babashka/http-client "0.4.19"]
                  ]
+  :jvm-opts ["--add-opens=java.base/java.nio=ALL-UNNAMED"
+             "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED"]
   :main ^:skip-aot accent.core
   :target-path "target/%s"
   :native-image {:name ""}
