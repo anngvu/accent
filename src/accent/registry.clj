0cc2ba4dd33c08921b755da918ada077ca07fccf
Anh Nguyet Vu <anngvu@gmail.com>
2025-06-05 09:45:02 -0700

    Sync registry tools

--- src/accent/registry.clj
+++ src/accent/registry.clj
@@ -553,6 +553,16 @@
   ;; Example usage
   
   ;; Define tools and prompts
+  (deftool :greet
+    "Greet a person"
+    {:type "object"
+     :properties {"name" {:type "string"}}
+     :required ["name"]}
+    :category #{:social}
+    :permissions #{:read}
+    
+    {:message (str "Hello, " (:name args) "!")})
+  
   (defprompt :analyze-code
     "Analyze code for improvements"
     :arguments [{:name "language" :description "Programming language" :required true}
