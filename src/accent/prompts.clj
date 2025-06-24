ef47ceb5693d918c02faf6833298e633638d5e2e
Anh Nguyet Vu <anngvu@gmail.com>
2025-06-24 08:49:38 -0700

    Add basic prompt

---
+++ src/accent/prompts.clj
@@ -0,0 +1,47 @@
+(ns accent.prompts
+  (:require [accent.registry :as registry]
+            [cheshire.core :as json]
+            [clj-http.client :as http]
+            [clojure.java.io :as io]))
+
+;; =============================================================================
+;; Dataset curation prompts
+;; =============================================================================
+
+;; File-level curation 
+
+(defn prep-dataset-manifest-handler
+  "Handle dynamic prompt for the dataset file manifest workflow"
+  [{:keys [folder_id data_coordinating_center]}] 
+  (let [prod "https://raw.githubusercontent.com/Sage-Bionetworks/data_curator_config/refs/heads/prod/"
+        config-url (str prod data_coordinating_center "/dca_config.json")] 
+    {:messages [{:role "user"
+               :content {:type "text"
+                         :text (str "Help me create metadata for files in dataset " folder_id " in Synapse. "
+                                    "For context, I know that my data coordination center is" data_coordinating_center ", which uses the specific configuration below. Help me understand the parameters as well if needed.")}}
+              {:role "user"
+               :content {:type "resource"
+                         :resource {:uri config-url
+                                    :text (slurp config-url)}}}
+              {:role "assistant"
+               :content {:type "text"
+                         :text (str "The first step is to generate a metadata manifest for `folder_id` " folder_id "(assuming the id given is indeed a qualifying Synapse folder)." 
+                                    "I will refer to the DCC-specific configuration for parameters like `asset_view`. "
+                                    "To infer `data_type` parameter for this dataset, I will check the data types/templates defined in the DCC data model. " 
+                                    "Then, looking at the dataset, if its type is not clear, my protocol suggests that I try to confirm the type by interviewing you about the data or sampling some files (with your permission and if I have the tools available). "
+                                    "Occasionally, the data model lacks coverage and doesn't define the appropriate template, in which case it's best to create an issue instead of using an incorrect template. "
+                                    "I will not proceed if I am not confident about the manifest I can generate for you. " 
+                                    "Let me attempt this approach now.")}}]}))
+
+(registry/defprompt :prep-dataset-manifest
+  "Workflow for creating metadata for Synapse dataset files according a specified standard. Workflow may also be called annotation or curation."
+  [{:name "folder_id"
+    :description "Synapse id of the dataset folder" 
+    :required true} 
+   {:name "data_coordinating_center" 
+    :enum ["ADKP" "AMP-AIM" "BTC" "CB" "EL" "GF" "HTAN" "HTAN2" "NF-OSI" "VEOIBD"] 
+    :description "The data coordinating center (DCC) in which this workflow should be placed in context." 
+    :required true}]
+  :category #{:synapse :curation}
+  :permissions #{:read}
+  :handler prep-dataset-manifest-handler)
--- src/server/mcp.clj
+++ src/server/mcp.clj
@@ -1,8 +1,9 @@
 (ns server.mcp
   (:gen-class)
   (:require [accent.state :refer [setup u]]
-            [accent.registry :refer [tool-registry deftool tools->mcp-format]]
+            [accent.registry :refer [tool-registry tools->mcp-format prompt-registry prompts->mcp-format]]
             [accent.tools :as tools]
+            [accent.prompts]
             [curate.synapse :refer [new-syn]]
             [io.modelcontext.clojure-sdk.stdio-server :as io-server]
             [com.brunobonacci.mulog :as mu]))
@@ -11,19 +12,25 @@
   (-> @tool-registry
       (tools->mcp-format)))
 
+(def all-prompts
+  (-> @prompt-registry
+      (prompts->mcp-format)))
+
 ;; TODO: assess state after setup to select and expose tools more intelligently.
 ;; For example, do not expose Synapse tools if user has not provided Synapse creds.
 ;; Currently exposes all tools in registry.
 
 (def accent-server-spec
   {:name "accent: AI-Assisted Curation/Content ENhancement Tools",
-   :version "0.7.0",
-   :tools all-tools})
+   :version "0.7.1",
+   :tools all-tools
+   :prompts all-prompts})
 
 (defn -main
   [& _args]
   (setup {:ui :external-client})
   (new-syn (@u :sat))
+  (alter-var-root #'tools/*schematic-auth-token* (constantly (@u :sat)))
   (let [server-id (random-uuid)]
     (mu/log ::mcp-server :info (str "Starting *accent* MCP server" server-id)) 
     @(io-server/run! (assoc accent-server-spec :server-id server-id))))
