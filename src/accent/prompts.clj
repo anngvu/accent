ab47169d610610628584987c5720ab9a4daee697
Anh Nguyet Vu <anngvu@gmail.com>
2025-06-24 08:59:32 -0700

    Add minor prompt revisions

--- src/accent/prompts.clj
+++ src/accent/prompts.clj
@@ -18,17 +18,17 @@
     {:messages [{:role "user"
                :content {:type "text"
                          :text (str "Help me create metadata for files in dataset " folder_id " in Synapse. "
-                                    "For context, I know that my data coordination center is" data_coordinating_center ", which uses the specific configuration below. Help me understand the parameters as well if needed.")}}
+                                    "For context, I know that my data coordination center is " data_coordinating_center ", which uses the specific configuration below. Help me understand the parameters as well if needed.")}}
               {:role "user"
                :content {:type "resource"
                          :resource {:uri config-url
                                     :text (slurp config-url)}}}
               {:role "assistant"
                :content {:type "text"
-                         :text (str "The first step is to generate a metadata manifest for `folder_id` " folder_id "(assuming the id given is indeed a qualifying Synapse folder)." 
+                         :text (str "The first step is to generate a metadata manifest for `folder_id` " folder_id ", assuming this is a qualifying Synapse folder. " 
                                     "I will refer to the DCC-specific configuration for parameters like `asset_view`. "
-                                    "To infer `data_type` parameter for this dataset, I will check the data types/templates defined in the DCC data model. " 
-                                    "Then, looking at the dataset, if its type is not clear, my protocol suggests that I try to confirm the type by interviewing you about the data or sampling some files (with your permission and if I have the tools available). "
+                                    "To apply the `data_type` parameter for this dataset, I will reference the data types/templates defined in the DCC data model. " 
+                                    "Looking at the dataset, if its type is not clear, my protocol suggests that I try to confirm the type by interviewing you about the data or sampling some files (with your permission and if I have the tools available). "
                                     "Occasionally, the data model lacks coverage and doesn't define the appropriate template, in which case it's best to create an issue instead of using an incorrect template. "
                                     "I will not proceed if I am not confident about the manifest I can generate for you. " 
                                     "Let me attempt this approach now.")}}]}))
