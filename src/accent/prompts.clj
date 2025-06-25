297acf51b03a3eb5f41897693f39636bb613d078
Anh Nguyet Vu <anngvu@gmail.com>
2025-06-24 18:03:35 -0700

    Add one more factor to prompt

--- src/accent/prompts.clj
+++ src/accent/prompts.clj
@@ -30,6 +30,7 @@
                                     "To apply the `data_type` parameter for this dataset, I will reference the data types/templates defined in the DCC data model. " 
                                     "Looking at the dataset, if its type is not clear, my protocol suggests that I try to confirm the type by interviewing you about the data or sampling some files (with your permission and if I have the tools available). "
                                     "Occasionally, the data model lacks coverage and doesn't define the appropriate template, in which case it's best to create an issue instead of using an incorrect template. "
+                                    "A possible issue is when files within the dataset are not homogenous, since mixed data types are not supported; the files would have to be reorganized into separate dataset folders first. "
                                     "I will not proceed if I am not confident about the manifest I can generate for you. " 
                                     "Let me attempt this approach now.")}}]}))
 
